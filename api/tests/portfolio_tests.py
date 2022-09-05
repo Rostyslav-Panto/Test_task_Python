c46894d7ed13e5cbfa2800abd15ce1bbd844ec9f
RostyslavPanto <rost.panto@gmail.com>
2022-09-05 13:41:00 +0300

    Add portfolio_tests.py file

---
+++ api/tests/portfolio_tests.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory, force_authenticate, APITestCase
+from rest_framework.utils.serializer_helpers import ReturnDict
+
+
+class TestAPI(APITestCase):
+    pass
